9184d1b1c08af3665a141361874d619ff8a0cdea
daniel <vermont.danielsouza@tokiomarine.com.br>
2022-04-20 13:27:51 -0300

    Tela de detalhes Pokemon

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,38 @@
+import { PokeApiService } from './../../service/poke-api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
 
-  constructor() { }
+  private urlPokemon: string = 'http://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'http://pokeapi.co/api/v2/pokemon-species';
+  public pokemon: any;
+  public isLoading: boolean = false;
 
   ngOnInit(): void {
+    this.getPokemon;
   }
 
+  get getPokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      this.pokemon = res;
+      this.isLoading = true;
+    });
+  }
 }
